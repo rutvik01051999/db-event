d6386b09282108448543629b8c67c9dd3284cf3b
rutvik <rutvik.mohite@bytestechnolab.com>
2024-08-02 19:23:01 +0530

    fdfdf

--- resources/views/user/event/form.blade.php
+++ resources/views/user/event/form.blade.php
@@ -7,25 +7,32 @@
         <form method="post" action="user/event/store">
             @csrf
 
-            @foreach ($data->personalinfo as $perinfo)
-                @if ($perinfo->option_types == 'input')
-                    <div class="form-group">
-                        <label for="formGroupExampleInput2">{{ $perinfo->name }}</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2"
-                            name="input_name{{ $perinfo->id }}" {{ $perinfo->required == 1 ? 'required' : '' }}>
-                    </div><br>
-                @elseif($perinfo->option_types == 'dropdown')
-                    <div class="form-group">
-                        <label for="inputState">{{ $perinfo->name }}</label>
-                        <select id="inputState" class="form-control" {{ $perinfo->required == 1 ? 'required' : '' }}>
-                            @foreach ($perinfo->options as $option)
-                                <option selected>{{ $option->name }}</option>
-                            @endforeach
-                        </select>
-                    </div><br>
-                @elseif($perinfo->option_types == 'checkbox')
-                    <div class="form-group">
-                        <label for="inputState">{{ $perinfo->name }}</label>
+        @foreach ($data->personalinfo as $perinfo)
+            @if($perinfo->option_types == "input")
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">{{$perinfo->name}}</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" name="input_name{{$perinfo->id}}"  {{$perinfo->required == 1 ? 'required': ''}}>
+                </div><br>
+
+            @elseif($perinfo->option_types == "number")
+                <div class="form-group">
+                <label for="formGroupExampleNumber2">{{$perinfo->name}}</label>
+                <input type="number" class="form-control" id="formGroupExampleNumber2" name="input_name{{$perinfo->id}}"  {{$perinfo->required == 1 ? 'required': ''}}>
+                </div><br>
+
+                @elseif($perinfo->option_types == "dropdown")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
+                    <select id="inputState" class="form-control" {{$perinfo->required == 1 ? 'required': ''}}>
+                    @foreach ($perinfo->options as $option)
+                        <option selected>{{$option->name}}</option>
+                    @endforeach
+                    </select>
+                </div><br>
+
+            @elseif($perinfo->option_types == "checkbox")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
 
                         @foreach ($perinfo->options as $option)
                             <div class="custom-control custom-checkbox">
@@ -54,25 +61,40 @@
             @endforeach
 
 
-            @foreach ($data->questions as $perinfo)
-                @if ($perinfo->option_types == 'input')
-                    <div class="form-group">
-                        <label for="formGroupExampleInput2">{{ $perinfo->name }}</label>
-                        <input type="text" class="form-control" id="formGroupExampleInput2"
-                            {{ $perinfo->required == 1 ? 'required' : '' }}>
-                    </div><br>
-                @elseif($perinfo->option_types == 'dropdown')
-                    <div class="form-group">
-                        <label for="inputState">{{ $perinfo->name }}</label>
-                        <select id="inputState" class="form-control" {{ $perinfo->required == 1 ? 'required' : '' }}>
-                            @foreach ($perinfo->options as $option)
-                                <option selected>{{ $option->name }}</option>
-                            @endforeach
-                        </select>
-                    </div><br>
-                @elseif($perinfo->option_types == 'checkbox')
-                    <div class="form-group">
-                        <label for="inputState">{{ $perinfo->name }}</label>
+        @foreach ($data->questions as $perinfo)
+            @if($perinfo->option_types == "input")
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">{{$perinfo->name}}</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2"  {{$perinfo->required == 1 ? 'required': ''}}>
+                </div><br>
+            
+                @elseif($perinfo->option_types == "number")
+                <div class="form-group">
+                <label for="formGroupExampleNumber2">{{$perinfo->name}}</label>
+                <input type="number" class="form-control" id="formGroupExampleNumber2" name="input_name{{$perinfo->id}}"  {{$perinfo->required == 1 ? 'required': ''}}>
+                </div><br>
+                
+
+            @elseif($perinfo->option_types == "dropdown")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
+                    <select id="inputState" class="form-control" {{$perinfo->required == 1 ? 'required': ''}}>
+                    @foreach ($perinfo->options as $option)
+                        <option selected>{{$option->name}}</option>
+                    @endforeach
+                    </select>
+                </div><br>
+            
+            @elseif($perinfo->option_types == "date")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
+                    <input type="text" name="event_end" class="form-control datepicker" placeholder="Enter ..."
+                    id="datepicker2">
+            </div><br>
+
+            @elseif($perinfo->option_types == "checkbox")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
 
                         @foreach ($perinfo->options as $option)
                             <div class="custom-control custom-checkbox">
@@ -82,27 +104,42 @@
                             </div>
                         @endforeach
 
-                    </div><br>
-                @elseif($perinfo->option_types == 'radio')
-                    <div class="form-group">
-                        <label for="inputState">{{ $perinfo->name }}</label>
+                </div><br>
 
-
-                        @foreach ($perinfo->options as $option)
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                                    value="option1" {{ $perinfo->required == 1 ? 'required' : '' }}>
-                                <label class="form-check-label" for="exampleRadios1">
-                                    {{ $option->name }}
-                                </label>
-                            </div>
-                        @endforeach
+            @elseif($perinfo->option_types == "radio")
+                <div class="form-group">
+                    <label for="inputState">{{$perinfo->name}}</label>
+                    @foreach ($perinfo->options as $option)
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" {{$perinfo->required == 1 ? 'required': ''}}>
+                            <label class="form-check-label" for="exampleRadios1">
+                                {{$option->name}}
+                            </label>
+                        </div>
+                    @endforeach
 
                     </div><br>
                 @endif
             @endforeach
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@section('content-js')
+<script>
+  $(document).ready(function () {
+   
+    $(function () {
+      $(".datepicker").datepicker({
+        minDate: 0,
+        dateFormat: 'yy-mm-dd',
+        onSelect: function (date) {
+          console.log(date)
+        }
+      });
+
+    });
+  });
+</script>
+@endsection
 @endsection
