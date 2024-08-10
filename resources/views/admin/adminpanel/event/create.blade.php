507eafe2cb140a37f2f08300fb0b08709baf5e95
rutvik <rutvik.mohite@bytestechnolab.com>
2024-08-10 19:10:32 +0530

    fdfdfd

--- resources/views/admin/adminpanel/event/create.blade.php
+++ resources/views/admin/adminpanel/event/create.blade.php
@@ -702,10 +702,10 @@
         $(addButton2).click(function () {
             //Check maximum number of input fields
             if (y < maxField) {
-                var fieldHTML = '<div id="removecls' + y +
+                var fieldHTML = '<div class="removecls2' + y +
                     '">  <div class="row "> <div class="col-sm-4"> <!-- text input --> <div class="form-group"> <label></label> <input type="text" name="p_quation[]" class="form-control quation" placeholder="Enter ..."> </div> </div> <div class="col-sm-2"> <div class="form-group"> <div class="form-group"> <label></label> <select class="form-control" name="p_required[]"> <option value="1">yes</option> <option value="0">no</option> </select> </div> </div> </div> <div class="col-sm-3"> <div class="form-group"> <label></label> <input type="text" name="p_option[]" class="form-control" placeholder="Enter ..." > </div> </div> <div class="col-sm-2"> <div class="form-group"> <label></label> <select class="form-control per_option' + y + '" name="p_option_type[]"></select> </div> </div> <div class="col-sm-1"> <div class="form-group"> <br> <svg xmlns="http://www.w3.org/2000/svg" data-id ="' +
                     y +
-                    '" width="32" height="32" fill="red" class="bi bi-x-circle-fill remove_button" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg></div> </div> </div></div>'; //New input field html 
+                    '" width="32" height="32" fill="red" class="bi bi-x-circle-fill remove_button2" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/></svg></div> </div> </div></div>'; //New input field html 
 
                 $(wrapper2).append(fieldHTML); //Add field html
 
@@ -725,8 +725,9 @@
         });
 
         // Once remove button is clicked
-        $(wrapper).on('click', '.remove_button', function (e) {
+        $(document).on('click', '.remove_button', function (e) {
             e.preventDefault();
+            console.log('enter')
             var id = $(this).attr("data-id");
             x--;
             $('#removecls' + id).remove();
@@ -734,6 +735,8 @@
 
         $(document).on('click', '.remove_button2', function (e) {
             var id = $(this).attr("data-id");
+            console.log(id)
+
             e.preventDefault();
             y--;
             $('.removecls2' + id).remove();
